6c5ae83ac17691142a30e60eebd55cd915efac88
Martijn The <martijnthe@users.noreply.github.com>
2013-10-16 20:50:25 -0700

    Update stgit.rb
    Updated stgit to 0.17.1

--- Library/Formula/stgit.rb
+++ Library/Formula/stgit.rb
@@ -1,9 +1,9 @@
 require 'formula'
 
 class Stgit < Formula
-  url 'http://download.gna.org/stgit/stgit-0.15.tar.gz'
+  url 'http://download.gna.org/stgit/stgit-0.17.1.tar.gz'
   homepage 'http://www.procode.org/stgit'
-  md5 'a4721b2a5f529cf5450109f9fcb4db19'
+  md5 '12e10e73276fa865098a23f032effd9b'
 
   def install
     system "make", "prefix=#{prefix}", "all"
